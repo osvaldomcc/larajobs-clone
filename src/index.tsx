74b94f83a8b648639c1e2876835ba418d1be3b0f
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-06-11 14:00:13 +0200

    Ready to start

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { store } from './app/store';
+import { store } from './store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import authReducer from '@/store/Auth/authSlice';
+import authReducer from './Auth/authSlice';
 
 export const store = configureStore({
   reducer: {
