1ea5cebcca936342e5ff10ee2a81177351260ccb
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-06-11 14:19:03 +0200

    Add colors file

---
+++ src/utils/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  black: '#2D3748',
+  darkBlue: '#2A4365',
+  formBackground: '#EDF2F7',
+  gray: '#E2E8F0',
+  green: '#75C19A',
+  lightBlue: '#30BCED',
+  lightestBlue: '#EBF8FF',
+  lightGray: '#F8FAFC',
+  red: '#F56565',
+  viveBlue: '#51A2E3',
+  white: '#FFFFFF',
+  lightYellow: '#FEFCBF',
+};
