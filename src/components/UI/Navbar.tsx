e3ba8cee682a09f5a5fb899b0e2d77fc68f39e26
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-06-14 13:46:30 +0200

    Create Navbar component

---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+
+import { Icon } from './Icon';
+import './Navbar.css';
+import Logo from '../../assets/logo.svg';
+
+interface Props {
+  className?: string;
+}
+
+export const Navbar: FC<Props> = ({ className }) => {
+  return (
+    <nav className={`navbar ${className}`}>
+      {/* Social */}
+      <div className="social">
+        <Icon name="rss" />
+        <Icon name="twitter" />
+        <button className="btn btn-social" type="button">
+          Partner
+        </button>
+        <button className="btn btn-social" type="button">
+          Account
+        </button>
+      </div>
+      {/* Logo */}
+      <picture className="logo-image">
+        <img src={Logo} alt="logo" height={40} />
+      </picture>
+      <div className="fill-space"></div>
+      {/* Post Job */}
+      <button type="button" className="btn btn-primary-outlined fixed-top">
+        Post a Job
+      </button>
+    </nav>
+  );
+};
