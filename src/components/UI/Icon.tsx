070dec85891a4c4035052d8cd9c700804e710c81
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-06-14 13:46:18 +0200

    Create Icon Component

---
+++ src/components/UI/Icon.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+
+interface Props {
+  name: string;
+  size?: number;
+}
+
+export const Icon: FC<Props> = ({ name, size = 24 }) => {
+  return (
+    <a href="#!">
+      <picture>
+        <img
+          src={require(`../../assets/icons/${name}.svg`)}
+          alt={`${name}-icon`}
+          height={size}
+          width={size}
+        />
+      </picture>
+    </a>
+  );
+};
