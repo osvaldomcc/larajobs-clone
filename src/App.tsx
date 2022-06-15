836592482e9e7d51a62141d619eab100ad400b79
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-06-15 17:46:50 +0200

    Structure the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Header } from "./components/UI/Header";
 import { PostJobAlert } from "./components/UI/PostJobAlert";
+import { Footer } from "./components/UI/Footer";
+import { Login } from "./pages/Login";
 import "./App.css";
 
 export const App = () => {
   return (
     <div className="app">
       <Header />
-      <main className="container"></main>
-      <PostJobAlert />
+      <main className="container">
+        <Login />
+      </main>
+      <PostJobAlert show={true} />
+      <Footer />
     </div>
   );
 };
