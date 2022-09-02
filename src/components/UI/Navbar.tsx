3b8336df4f2bf889d7655a89cbe6b41c1afe7f52
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-09-02 08:13:00 -0400

    Crate the app navbar

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,36 +1,59 @@
 import { FC } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Icon } from "./Icon";
-import "./css/Navbar.css";
 import Logo from "../../assets/logo.svg";
+import "./css/Navbar.css";
 
 interface Props {
   className?: string;
+  hidePostJob?: boolean;
 }
 
-export const Navbar: FC<Props> = ({ className }) => {
+export const Navbar: FC<Props> = ({ className, hidePostJob }) => {
+  const navigate = useNavigate();
+
+  const handlePostJob = () => {
+    navigate("/post-a-job");
+  };
+
   return (
-    <nav className={`navbar ${className}`}>
-      {/* Social */}
-      <div className="social">
-        <Icon name="rss" />
-        <Icon name="twitter" />
-        <button className="btn btn-social" type="button">
-          Partner
-        </button>
-        <button className="btn btn-social" type="button">
-          Account
-        </button>
-      </div>
-      {/* Logo */}
-      <picture className="logo-image">
+    <>
+      <nav className={`navbar ${className}`}>
+        {/* Social */}
+        <div className="social">
+          <Icon name="rss" />
+          <Icon name="twitter" />
+          <button className="btn btn-social" type="button">
+            Partner
+          </button>
+          <Link to="/login">
+            <button className="btn btn-social" type="button">
+              Account
+            </button>
+          </Link>
+        </div>
+        {/* Logo */}
+        <Link to="/">
+          <picture className="logo-image">
+            <img src={Logo} alt="logo" height={40} />
+          </picture>
+        </Link>
+        <div className="fill-space"></div>
+        {/* Post Job */}
+        {!hidePostJob && (
+          <button
+            onClick={handlePostJob}
+            type="button"
+            className="btn btn-primary-outlined fixed-top"
+          >
+            Post a Job
+          </button>
+        )}
+      </nav>
+      <picture className="logo">
         <img src={Logo} alt="logo" height={40} />
       </picture>
-      <div className="fill-space"></div>
-      {/* Post Job */}
-      <button type="button" className="btn btn-primary-outlined fixed-top">
-        Post a Job
-      </button>
-    </nav>
+    </>
   );
 };
