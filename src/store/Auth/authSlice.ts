d2cb91b42fbe0107d28649ff8e7d960c7f73fc28
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-09-02 08:10:08 -0400

    Create slice of the store

--- src/store/Auth/authSlice.ts
+++ src/store/Auth/authSlice.ts
@@ -1,15 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: {},
 };
 
 export const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {},
 });
-
-// export const { } = authSlice.actions;
 
 export default authSlice.reducer;
