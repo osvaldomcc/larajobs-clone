ae23e3d1f322d01b13dddc6eda3ff2e027ed4259
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-09-02 08:12:50 -0400

    Create input component

--- src/components/UI/InputText.tsx
+++ src/components/UI/InputText.tsx
@@ -1,37 +1,48 @@
 import { FC } from "react";
+import { ErrorMessage, useField } from "formik";
+
 import "./css/InputText.css";
 
 interface Props {
   name: string;
   id: string;
-  type: "tel" | "text" | "search" | "url" | "password" | "email";
+  type: "tel" | "text" | "search" | "url" | "password" | "email" | "file";
   label?: string;
+  sublabel?: string;
   value?: string | number | readonly string[] | undefined;
   placeholder?: string;
+  className?: string;
+  hint?: string;
+  [key: string]: any;
 }
 
 export const InputText: FC<Props> = ({
-  name,
-  id,
-  type,
   label,
-  value,
-  placeholder,
+  sublabel,
+  hint,
+  className,
+  ...props
 }) => {
+  const [field] = useField(props);
+
   return (
     <div>
       {label && (
-        <label htmlFor={id} className="form-label text-black">
-          {label}
+        <label htmlFor={props.id} className="form-label text-black">
+          {label} <span className="form-sublabel">{sublabel}</span>
         </label>
       )}
       <input
-        type={type}
-        name={name}
-        id={id}
-        value={value}
-        className="form-input"
-        placeholder={placeholder}
+        autoComplete="off"
+        className={`form-input ${className}`}
+        {...field}
+        {...props}
+      />
+      <p className="text-dark-gray">{hint}</p>
+      <ErrorMessage
+        name={props.name}
+        className="form-error-item"
+        component="div"
       />
     </div>
   );
